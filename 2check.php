ae12eb52d63c812f90289f9e0a9163ae976649cd
Walter Garcia <wgarcia6508@gmail.com>
2019-11-25 22:00:40 -0600

    agregue html a wcheck

--- 2check.php
+++ 2check.php
@@ -1,5 +1,32 @@
-<?php $ttYY=2; 
+<?php 
+$ttYY=2; 
 $idT=7;
 								
 echo "$".$ttYY;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <head>
+
+    </head>
+
+    <H1><?php echo "$".$ttYY;
+?></H1>
+
+
+<body>
+   
+
+    
+
+</body>
+
+</html>
