f4d6cba0415c6deedd4ee18c6269cff1ba8fa881
Walter Garcia <wgarcia6508@gmail.com>
2019-11-26 21:47:03 -0600

    guardar cambios locales

--- 2check.php
+++ 2check.php
@@ -24,7 +24,44 @@ echo "$".$ttYY;
 
 <body>
    
-
+<form action = 'https: //www.2checkout.com/checkout/purchase' method = 'post'>
+<input type = '' name = 'sid' value = '1303908'>
+<input type = '' name = 'mode' value = '2CO'>
+<input type = '' name = 'li_0_type' value = 'product'>
+<input type = '' name = 'li_0_name' value = 'Ejemplo de nombre del producto'>
+<input type = '' name = 'li_0_product_id' value = 'ID de producto de ejemplo'>
+<input type = '' name = 'li_0__description' value = 'Ejemplo de descripción del producto'>
+<input type = '' name = 'li_0_price' value = '10 .00 '>
+<input type = '' name = 'li_0_quantity' value = '2'>
+<input type = '' name = 'li_0_tangible' value = 'Y'>
+<input type = '' name = 'li_1_type' value = 'envío'>
+<input type = '' name = 'li_1_name' value = 'Ejemplo de método de envío'>
+<input type = '' name = 'li_1_price' value = '1.50'>
+<input type = '' name = 'li_2_type' value = 'cupón'>
+<input type = '' name = 'li_2_name' value = 'Cupón de ejemplo'>
+<input type = '' name = 'li_2_price' value = '1.00'>
+<input type = '' name = 'li_3_type' value = 'tax'>
+<input type = '' name = 'li_3_name' value = 'Ejemplo de impuesto'>
+<input type = '' name = 'li_3_price' value = '0.50'>
+<input type = '' name = 'card_holder_name' value = 'Checkout Shopper'>
+<input type = '' name = 'street_address' value = '123 Test St'>
+<input type = '' name = 'street_address2' value = 'Suite 200'>
+<input type = '' name = 'city' value = 'Columbus'>
+<input type = '' name = 'state' value = 'OH'>
+<input type = '' name = 'zip' value = '43228'>
+<input type = '' name = 'country' value = 'USA'>
+<input type = '' name = 'email' value='example@2co.com '>
+<input type = '' name = 'phone' value = '614-921-2450'>
+<input type = '' name = 'phone_extension' value = '197'>
+<input type = '' name = 'ship_name' value = 'Receptor de regalo'>
+<input type = '' name = 'ship_street_address' value = '1234 Address Road'>
+<input type = '' name = 'ship_street_address2' value = 'Apartamento 123'>
+<input type = '' name = 'ship_city' value = 'Columbus'>
+<input type = '' name = 'ship_state' value = 'OH'>
+<input type = '' name = 'ship_zip' value = '43235'>
+<input type = '' name = 'ship_country' value = 'EE. UU.'>
+<input name = 'submit' type = 'submit' value = 'Checkout'>
+</form>
     
 
 </body>
